cb1bca85f507b055801a826640fc262679f4bf2a
seis-635-fall2020-Jie <yang1014@stthomas.edu>
2020-11-13 18:57:24 -0600

    First Commit.

---
+++ CrapsSimulation/src/CrookedDie1.java
@@ -0,0 +1,24 @@
+
+public class CrookedDie1 extends Die
+{
+	public CrookedDie1()
+	{
+		super();
+		//I don't have to write this super();
+		//It automatically inserted by the Java Complier.
+		//Can't skip calling the constructor of the super class before return the to the subclass.
+		//Because sometimes, the constructor of a subclass depends on the superclass.
+		CrapsGame.println("  CrookedDie1's constructor called.");
+	}
+
+	public int getLastRoll() // this OVERRIDES (replaces) Die's getLastRoll()
+	{
+		return 3;
+	}
+
+	public String toString() // this OVERRIDES Die's toString()
+	{
+		return "A CrookedDie1 always rolling 3, and... "+ " " + super.toString();
+	}
+
+}
